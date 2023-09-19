6df7b8b77d2be3f5d0ea87e03040dfc2bb031e85
abhijith9770 <iamabhijiths9770@gmail.com>
2023-09-19 17:12:04 +0530

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,123 @@
+// day 1 - 13 sep
+// introductions
+
+// day 2 - sep 14
+// installations
+
+// day 3 - sep 15
+
+// void main() {
+// String name = "abhi";
+// int age = 21;
+// int phone = 09876754321;
+// double cgpa = 8.9;
+// print("my name is $name");
+// print("I'm $age years old");
+// print("My phone number is $phone");
+// print("i have a CGPA of $cgpa");
+
+// variables = to store values in the programming
+
+// synatx => datatype - variable+_name = value;
+
+// variable name is the name given to the memory location where we store the value
+
+// day 4 - sep 18
+
+/// variables are used to store values ina program
+/// to create a variable
+/// synatx >> datatype - variable_name = value ;
+/// variable name is the name given to memory location where w store values
+
+// DYNAMIC
+//--------\\
+
+// dynamic classroom = "Android - flutter";
+// print(classroom);
+
+// in dynamic the datatype is changed when the value of the variable is changed. it can be changed as many as we want.
+
+// VAR
+//----\\
+
+// var name = "abhi";
+// print(name);
+
+// CREATING A CLASS
+//-----------------\\
+// }
+
+// class Students {
+//   String? name;
+//   int? age;
+//   int? phone;
+//   dynamic email;
+//   dynamic qualification;
+// }
+
+// class students {
+//   String? name;
+//   int? age;
+//   int? phone;
+//   String? email;
+//   String? qualification;
+// }
+
+// void main() {
+// // // object creation class name ,object_name= classname();- constructor
+//   students st1 = students();
+// // // instance variables vcan be access outside the class only using by object
+
+// print("student 1 name =${st1.name = "abhi"}");
+//   print("student 1 age =${st1.age = 21}");
+//   print("student 1 phone =${st1.phone = 0987654322}");
+//   print("student 1 email =${st1.email = "arun628287@gmail.com"}");
+//   print("student 1 qualification = ${st1.qualification = "bsc"}");
+
+//   print("------------------------------------------------");
+
+//   students st2 = students();
+
+// // // instance variables vcan be access outside the class only using by object
+
+//   print("student 2 name =${st2.name = "karthi"}");
+//   print("student 2 age =${st2.age = 20}");
+//   print("student 2 phone =${st2.phone = 0987654321}");
+//   print("student 2 email =${st2.email = "arun628287@gmail.com"}");
+//   print("student 2 qualification = ${st2.qualification = "bsc"}");
+// }
+
+// class cars {
+//   String? name;
+//   int? price;
+//   String? brand;
+//   double? mileage;
+//   dynamic colour;
+// }
+
+// void main() {
+//   cars ct1 = cars();
+//   print("Nmae of the car = ${ct1.name = "INNOVA"}");
+//   print("Price of the car is ${ct1.price = 1400000}");
+//   print("Brand of the car is ${ct1.brand = "TOYOTA"}");
+//   print("The car has a mileage of ${ct1.mileage = 14.5}");
+//   print("It has 3 colour variants${ct1.colour = " red,blue and cream"}");
+
+//   print("***********************************************************");
+
+//   cars ct2 = cars();
+//   print("Nmae of the car = ${ct2.name = "I8"}");
+//   print("Price of the car is ${ct2.price = 8400000}");
+//   print("Brand of the car is ${ct2.brand = "BMW"}");
+//   print("The car has a mileage of ${ct2.mileage = 14.5}");
+//   print("It has 3 colour variants${ct2.colour = " red and blue"}");
+
+//   print("***********************************************************");
+
+//   cars ct3 = cars();
+//   print("Nmae of the car = ${ct3.name = "ED500"}");
+//   print("Price of the car is ${ct2.price = 6400000}");
+//   print("Brand of the car is ${ct2.brand = "MERCEDES BENZ"}");
+//   print("The car has a mileage of ${ct2.mileage = 17.5}");
+//   print("It has 3 colour variants${ct2.colour = " Back and Navy blue"}");
+// }
